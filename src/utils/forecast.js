2a16d44e8e7fe332e95f3c81cd0d728f31a26290
unknown <bhawnabhatia785@gmail.com>
2020-03-21 21:09:39 +0530

    Add info in forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast=(latitude,longitude,callback)=>{
         }else if(body.error){
             callback('Unable to find given location',undefined)
         }else{
-            callback(undefined,body.daily.data[0].summary + 'It is currently ' + body.currently.temperature + ' degrees out . There is a ' + body.currently.precipProbability + '% chance of rain')
+            callback(undefined,body.daily.data[0].summary + ' Maximum Temperature today is: ' + body.daily.data[0].temperatureHigh + ' degrees, Minimum Temperature today is: ' + body.daily.data[0].temperatureLow + ' degrees. It is currently ' + body.currently.temperature + ' degrees out . There is a ' + body.currently.precipProbability + '% chance of rain')
             
         }
     })
